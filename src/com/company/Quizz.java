bd1a32a62e22622f123a650fd5ba1508bfad5d3f
TomLinper <tomas.linper@gmail.com>
2021-05-09 00:47:51 +0100

    Falta Print Quizz

---
+++ src/com/company/Quizz.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Quizz extends Publicacao {
+    //Variaveis de instancia.
+    private String titulo;
+    private int nPerguntas;
+    private ArrayList<Pergunta> perguntas;
+
+    /**
+     * Construtor da classe Quizz.
+     * @param autor Autor da publicacao.
+     * @param data Data de criacao da publicacao.
+     * @param visibilidade Visibilidade da publicacao.
+     * @param titulo Titulo do Quizz.
+     * @param nPerguntas Numero de perguntas do quizz.
+     */
+    public Quizz(String autor, LocalDate data, boolean visibilidade, String titulo, int nPerguntas) {
+        super(autor, data, visibilidade);
+        this.titulo = titulo;
+        this.nPerguntas = nPerguntas;
+        this.perguntas = new ArrayList<>(nPerguntas);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getnPerguntas() {
+        return nPerguntas;
+    }
+
+    public void addPergunta(Pergunta pergunta) {
+        perguntas.add(pergunta);
+    }
+
+    @Override
+    public String getNome() {
+        return null;
+    }
+
+    @Override
+    public String getExtensao() {
+        return null;
+    }
+
+    @Override
+    public String getCorpo() {
+        return null;
+    }
+}
